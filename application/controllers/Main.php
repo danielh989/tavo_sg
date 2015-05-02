4e71391f14293d3138a6b0a8f13682b46a566394
Pedro Flores <pflores2704@gmail.com>
2015-05-02 02:03:11 -0430

    Agregando comentarios y algunas mejoras en el codigo

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,81 +3,119 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller
 {
-    
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('main_model');
-        $this->load->helper(array('url'));
-        $this->load->library('session');
-    }
-    
-    public function index() {
-        $data['mesas_activas'] = $this->main_model->mesas_activas();
-        $this->load->view('main', $data);
-    }
-    
-    public function getMesas() {
-        echo json_encode($this->main_model->mesas_libres());
-    }
-    
-    public function getCategorias() {
-        echo json_encode($this->main_model->categorias());
-    }
-    
-    public function getProductosXCategoria() {
-        $id = $this->input->post('id_cat');
-        echo json_encode($this->main_model->productos_x_categoria($id));
-    }
-    
-    public function crear_pedido() {
-        $productos = json_decode(json_encode($_POST['productos']));
-        $this->main_model->crear_pedido($_POST['id_mesa'], $productos);
-    }
-    
-    public function productos() {
-        
-        $data['productos'] = $this->main_model->listar_productos();
-        $this->load->view('productos', $data);
-    }
-    
-    public function pedido($id_pedido) {
-        // Detalles del pedido
-        $data['detalle'] = $this->main_model->detalle_pedido($id_pedido);
-        $data['devoluciones'] = $this->main_model->productos_devueltos($id_pedido);
-        $this->session->set_flashdata('id_pedido', $id_pedido);
+ /**
+	* Inicializa modelos, helpers y librerias
+	*
+	* @return void 
+	*/
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('main_model');
+    $this->load->helper(array('url'));
+    $this->load->library('session');
+  }
+  
+  /**
+  * Carga la vista con las mesas con ordenes activas
+  *
+  * @return void
+  */
+  public function index() {
+    $data['mesas_activas'] = $this->main_model->mesas_activas();
+    $this->load->view('main', $data);
+  }
+ 
+  /**
+	* Retorna las mesas disponibles
+	*
+	* @return json object
+	*/
+  public function getMesas() {
+    $this->encode($this->main_model->mesas_libres());
+  }
 
+  /**
+	* Retorna las categorias
+	*
+	* @return json object
+	*/
+  public function getCategorias() {
+    $this->encode($this->main_model->categorias());
+  }
+  
+  /**
+	* Retorna los productos de una categoria
+	*
+	* @return json object
+	*/
+  public function getProductosXCategoria() {
+    $id = $this->input->post('id_cat');
+    $this->encode($this->main_model->productos_x_categoria($id));
+  }
+  
+  public function crear_pedido() {
+    $productos = json_decode(json_encode($_POST['productos']));
+    $this->main_model->crear_pedido($_POST['id_mesa'], $productos);
+  }
+  
+  public function productos() {
+    $data['productos'] = $this->main_model->listar_productos();
+    $this->load->view('productos', $data);
+  }
+  
+  /**
+  * Carga la vista de un pedido con sus detalles
+  *
+  * @param $id_pedido ID del pedido
+  * @return void
+  */
+  public function pedido($id_pedido) {
+    // Detalles del pedido
+    $data['detalle'] = $this->main_model->detalle_pedido($id_pedido);
+    $data['devoluciones'] = $this->main_model->productos_devueltos($id_pedido);
+    $this->session->set_flashdata('id_pedido', $id_pedido);
 
-        // Total acumulado del pedido
-        $total = $this->main_model->total_pedido($id_pedido);
-        $data['total'] = explode('.', $total);
-         // Formateo del precio para la vista
-        $data['total'] = array_filter($data['total']);
-        // Productos del pedido
-        $data['productos'] = $this->main_model->productos_pedido($id_pedido);
-        
-        $this->load->view('pedido', $data);
-    }
+    // Total acumulado del pedido
+    $total = $this->main_model->total_pedido($id_pedido);
+    $data['total'] = explode('.', $total);
+     // Formateo del precio para la vista
+    $data['total'] = array_filter($data['total']);
+    // Productos del pedido
+    $data['productos'] = $this->main_model->productos_pedido($id_pedido);
     
-    public function agregar_producto() {
-        print_r($_POST);
-    }
-    
-    public function eliminar_producto_pedido() {
-        $this->main_model->eliminar_producto_pedido($this->input->post('id_pedido'), $this->input->post('id_producto'));
-    }
+    $this->load->view('pedido', $data);
+  }
+  
+  public function agregar_producto() {
+    print_r($_POST);
+  }
+  
+  public function eliminar_producto_pedido() {
+    $this->main_model->eliminar_producto_pedido($this->input->post('id_pedido'), $this->input->post('id_producto'));
+  }
 
-    public function eliminar_producto_devuelto() {
-        $this->main_model->eliminar_producto_devuelto($this->input->post('id_pedido'), $this->input->post('id_producto'));
-    }
-    
-    public function devolver_producto_pedido() {
-        $this->main_model->devolver_producto_pedido($this->input->post('id_pedido'), $this->input->post('id_producto'));
-    }
+  public function eliminar_producto_devuelto() {
+    $this->main_model->eliminar_producto_devuelto($this->input->post('id_pedido'), $this->input->post('id_producto'));
+  }
+  
+  public function devolver_producto_pedido() {
+    $this->main_model->devolver_producto_pedido($this->input->post('id_pedido'), $this->input->post('id_producto'));
+  }
 
-    public function pagar_pedido() {
-        $efectivo = $this->input->post('efectivo');
-        $debito = $this->input->post('debito');
-        $id_pedido = $this->session->flashdata('id_pedido');
-        $this->main_model->pagar_pedido($id_pedido,$efectivo,$debito);
-    }
+  public function pagar_pedido() {
+    $efectivo = $this->input->post('efectivo');
+    $debito = $this->input->post('debito');
+    $id_pedido = $this->session->flashdata('id_pedido');
+    $this->main_model->pagar_pedido($id_pedido,$efectivo,$debito);
+  }
+
+  /**
+  * Convertir datos en un objeto JSON
+  *
+  * @return json objeto
+  */
+  private function encode($data){
+  	header('Content-type: application/json');
+  	print json_encode($data);
+  }
 }
--- assets/js/tavo.js
+++ assets/js/tavo.js
@@ -1,292 +1,325 @@
+/*!
+* Tavo SG JavaScript
+* 
+* @author Jesus Torres
+* @author Daniel Arroy
+* @author Pedro Flores
+*
+* Copyright 20015 Lab 707.
+*
+* Date: 02/05/2015
+*/
 $(function() {
     mesasDisponibles();
     gestionarProductos();
     gestionarPedidos();
 });
-
+/**
+* Gestionar los detalles de un pedido
+* (Agregar productos, cambiar mesa, pagar)
+*/
 function gestionarPedidos() {
-    $('.btn-eliminar').on('click', function() {
-        var type = 'post',
-            url = '/tavo_sg/main/eliminar_producto_pedido',
-            data = {};
-        data['id_pedido'] = $(this).data('pedido');
-        data['id_producto'] = $(this).data('producto');
-        console.log(data);
-        $.ajax({
-            type: type,
-            url: url,
-            data: data,
-            success: function(response) {
-                location.reload();
-            }
-        });
-    });
-
-
-     $('.btn-eliminar-devuelto').on('click', function() {
-        var type = 'post',
-            url = '/tavo_sg/main/eliminar_producto_devuelto',
-            data = {};
-        data['id_pedido'] = $(this).data('pedido');
-        data['id_producto'] = $(this).data('producto');
-        console.log(data);
-        $.ajax({
-            type: type,
-            url: url,
-            data: data,
-            success: function(response) {
-                location.reload();
-            }
-        });
-    });
-
-
-    $('.btn-devolver').on('click', function() {
-        var type = 'post',
-            url = '/tavo_sg/main/devolver_producto_pedido',
-            data = {};
-        data['id_pedido'] = $(this).data('pedido');
-        data['id_producto'] = $(this).data('producto');
-        $.ajax({
-            type: type,
-            url: url,
-            data: data,
-            success: function(response) {
-                location.reload();
-            }
-        });
-    });
-}
-
-
-
-
-
-
-
-
-function getCategorias(select, auto, categoria) {
+  $('.btn-eliminar').on('click', function() {
     var type = 'post',
-        url = '/tavo_sg/main/getCategorias',
+        url = '/tavo_sg/main/eliminar_producto_pedido',
         data = {};
+    data['id_pedido'] = $(this).data('pedido');
+    data['id_producto'] = $(this).data('producto');
+    console.log(data);
     $.ajax({
-        type: type,
-        url: url,
-        data: data,
-        success: function(response) {
-            json = jQuery.parseJSON(response);
-            sel = $(select);
-            sel.empty();
-            sel.append('<option value="0">Seleccionar...</option>');
-            $.each(json, function(index, value) {
-                sel.append('<option value"' + json[index].id + '">' + json[index].nombre + '</option>');
-            });
-            if (auto == 1) {
-                sel.val(categoria);
-            }
-        }
+      type: type,
+      url: url,
+      data: data,
+      success: function(response) {
+        location.reload();
+      }
     });
+  });
+
+  $('.btn-eliminar-devuelto').on('click', function() {
+    var type = 'post',
+        url = '/tavo_sg/main/eliminar_producto_devuelto',
+        data = {};
+    data['id_pedido'] = $(this).data('pedido');
+    data['id_producto'] = $(this).data('producto');
+    console.log(data);
+    $.ajax({
+      type: type,
+      url: url,
+      data: data,
+      success: function(response) {
+        location.reload();
+      }
+    });
+  });
+
+
+  $('.btn-devolver').on('click', function() {
+    var type = 'post',
+        url = '/tavo_sg/main/devolver_producto_pedido',
+        data = {};
+    data['id_pedido'] = $(this).data('pedido');
+    data['id_producto'] = $(this).data('producto');
+    $.ajax({
+      type: type,
+      url: url,
+      data: data,
+      success: function(response) {
+        location.reload();
+      }
+    });
+  });
 }
 
+/**
+* Cargar categorias en un select (Generica)
+*
+* @param select String 
+* @param auto BOOL
+* @param categoria String
+* 
+* @return void
+*/
+function getCategorias(select, auto, categoria) {
+  var type = 'post',
+      url = '/tavo_sg/main/getCategorias',
+      data = {};
+  $.ajax({
+    type: type,
+    url: url,
+    data: data,
+    success: function(response) {
+      json = jQuery.parseJSON(response);
+      sel = $(select);
+      sel.empty();
+      sel.append('<option value="0">Seleccionar...</option>');
+      $.each(json, function(index, value) {
+          sel.append('<option value"' + json[index].id + '">' + json[index].nombre + '</option>');
+      });
+      if (auto == 1) {
+          sel.val(categoria);
+      }
+    }
+  });
+}
 
-
-
-
-
-
-
+/*
+* Gestionar los productos a nivel de Administrador
+*/
 function gestionarProductos() {
-    // Abrir el modal para agregar producto
-    $('.btn-agregar').on('click', function() {
-        getCategorias('#categorias', 0);
-        $('input').val("");
-        $('textarea').val("");
+  // Abrir el modal para agregar producto
+  $('.btn-agregar').on('click', function() {
+      getCategorias('#categorias', 0); // Dejar el select en opcion 0
+      $('input').val(""); // Borrar contenido los inputs
+      $('textarea').val(""); // Borrar contenido del textarea
+  });
+
+  // Enviar datos para almacenarlos en la DB (Agregar Pedido)
+  $('.btn-guardar').on('click', function() {
+    var form = $('form#producto'),
+        type = form.attr('method'),
+        url = form.attr('action'),
+        data = {};
+
+    form.find('[name]').each(function(index, value) {
+      var that = $(this),
+          name = that.attr('name'),
+          value = that.val();
+      
+      data[name] = value; // Guardamos datos del formulario
     });
-    // Enviar datos para almacenarlos en la DB
-    $('.btn-guardar').on('click', function() {
-        var form = $('form#producto'),
-            type = form.attr('method'),
-            url = form.attr('action'),
-            data = {};
-        form.find('[name]').each(function(index, value) {
-            var that = $(this),
-                name = that.attr('name'),
-                value = that.val();
-            data[name] = value;
-        });
-        $.ajax({
-            type: type,
-            url: url,
-            data: data,
-            success: function(response) {
-                console.log(response);
-            }
-        });
+
+    $.ajax({
+      type: type,
+      url: url,
+      data: data,
+      success: function(response) {
+          console.log(response);
+      }
     });
-    // Editar un producto
-    $('.table tbody .btn-editar').on('click', function() {
-        var fila = $(this).closest('tr'),
-            id = fila.data('id-producto'),
-            cat = fila.find('.categoria').text();
-        $('input[name=id]').val(id);
-        $('input[name=nombre]').val(fila.find('.nombre').text());
-        $('input[name=precio]').val(fila.find('.precio').text());
-        $('textarea[name=descripcion]').val(fila.find('.descripcion').text());
-        getCategorias('#categorias', 1, cat);
-    });
-    // Eliminar producto
-    $('.table tbody .btn-eliminar').on('click', function() {
-        var row = $(this).closest('tr'),
-            idProducto = row.data('id-producto');
-        $('#eliminarProducto a').attr('href', 'eliminar/' + idProducto);
-    });
+  });
+
+  // Editar un producto
+  $('.table tbody .btn-editar').on('click', function() {
+      var fila = $(this).closest('tr'),
+          id = fila.data('id-producto'),
+          cat = fila.find('.categoria').text();
+      $('input[name=id]').val(id);
+      $('input[name=nombre]').val(fila.find('.nombre').text());
+      $('input[name=precio]').val(fila.find('.precio').text());
+      $('textarea[name=descripcion]').val(fila.find('.descripcion').text());
+      getCategorias('#categorias', 1, cat);
+  });
+  // Eliminar producto
+  $('.table tbody .btn-eliminar').on('click', function() {
+      var row = $(this).closest('tr'),
+          idProducto = row.data('id-producto');
+      $('#eliminarProducto a').attr('href', 'eliminar/' + idProducto);
+  });
 }
 
-
-
-
-
-
-
-
+/**
+* Generar un nuevo pedido
+*/
 function mesasDisponibles() {
-    var pedido = {
-        "id_mesa": "",
-        "productos": []
-    };
-    var categorias = {};
-    $('#myModal').on('hidden.bs.modal', function() {
-        pedido = {
-            "id_mesa": "",
-            "productos": []
-        };
-    });
-    // Funcion para traer las mesas disponibles
-    $('.table-add').on('click', function() {
-        var that = $(this),
-            url = 'index.php/main/getMesas',
-            type = 'get',
-            data = {};
-        $.ajax({
-            url: url,
-            type: type,
-            data: data,
-            success: function(response) {
-                json = jQuery.parseJSON(response);
-                var template = $('#select-table').html();
-                $('.modal-body').html("");
-                $.each(json, function(index, value) {
-                    $('.modal-body').append(Mustache.render(template, json[index]));
-                });
-                $('.modal-footer').html("");
-            }
-        });
-    });
-    // Funcion para traer categorias
-    $('.modal-body').on('click', '.mesa-libre', function() {
-        var that = $(this),
-            url = 'index.php/main/getCategorias',
-            type = 'post',
-            data = {};
-        // Guardando el ID de la mesa seleccionada
-        pedido.id_mesa = that.data('id');
-        console.log(pedido);
-        $.ajax({
-            url: url,
-            type: type,
-            data: data,
-            success: function(response) {
-                categorias = jQuery.parseJSON(response);
-                var template = $('#categorias-template').html(),
-                    btnTemplate = $('#btn-completar').html();
-                $('.modal-body').html("");
-                $.each(categorias, function(index, value) {
-                    $('.modal-body').append(Mustache.render(template, categorias[index]));
-                });
-                $('.modal-footer').html("");
-                $('.modal-footer').append(Mustache.render(btnTemplate));
-            }
-        });
-    });
-    // Funcion para traer los productos de una categoria
-    $('.modal-body').on('click', '.categoria', function() {
-        var that = $(this),
-            url = 'index.php/main/getProductosXCategoria',
-            type = 'post',
-            data = {};
-        // ID de la categoria seleccionada
-        data['id_cat'] = $(this).data('id');
-        $.ajax({
-            url: url,
-            type: type,
-            data: data,
-            success: function(response) {
-                json = jQuery.parseJSON(response);
-                var template = $('#productos-template').html(),
-                    back = $('#back-button').html();
-                $('.modal-body').html("");
-                $('.modal-body').append(back);
-                $.each(json, function(index, value) {
-                    $('.modal-body').append(Mustache.render(template, json[index]));
-                });
-            }
-        });
-    });
-    $('.modal-body').on('click', '.btn-agregar', function() {
-        var that = $(this),
-            flag = 0,
-            cantidad = that.closest('.number-wrapper').find('.cantidad'),
-            valor = parseInt(cantidad.text());
-        cantidad.html("");
-        if (pedido.productos == '') {
-            pedido.productos.push({
-                "id": that.data('id'),
-                "cantidad": "1"
-            });
-            cantidad.append("1");
-        } else {
-            // Buscamos el ID en el arreglo de productos,
-            // si existe, se le suma uno a la cantidad.
-            $.each(pedido.productos, function(index, value) {
-                // Si el ID del producto existe en el arreglo
-                // sumale uno a la cantidad
-                if (pedido.productos[index].id === that.data('id')) {
-                    pedido.productos[index].cantidad = parseInt(pedido.productos[index].cantidad) + 1;
-                    pedido.productos[index].id = that.data('id');
-                    cantidad.append(valor + 1);
-                    flag = 1;
-                }
-            });
-            // Si el ID no existe en el arreglo, agregamos un
-            // producto nuevo
-            if (flag == 0) {
-                pedido.productos.push({
-                    "id": that.data('id'),
-                    "cantidad": "1"
-                });
-                cantidad.append("1");
-            }
-        }
-    });
-    $('.modal-body').on('click', '.btn-atras', function() {
-        var template = $('#categorias-template').html();
-        var json = $.merge(categorias, pedido);
+  var pedido = {
+      "id_mesa": "",
+      "productos": []
+  };
+
+  var categorias = {};
+
+  // Borrar pedido al cerrar el modal
+  $('#myModal').on('hidden.bs.modal', function() {
+      pedido = {
+          "id_mesa": "",
+          "productos": []
+      };
+  });
+
+  // Funcion para traer las mesas disponibles
+  $('.table-add').on('click', function() {
+    var that = $(this),
+        url = 'main/getMesas',
+        type = 'get',
+        data = {};
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      success: function(mesas) {
+        var template = $('#select-table').html();
+
         $('.modal-body').html("");
-        $.each(categorias, function(index, value) {
-            $('.modal-body').append(Mustache.render(template, json[index]));
+        $.each(mesas, function(index, value) {
+            $('.modal-body').append(Mustache.render(template, mesas[index]));
         });
+        $('.modal-footer').html("");
+      }
     });
-    $('.modal-footer').on('click', '.btn-completar', function() {
-        var type = 'post',
-            url = 'index.php/main/crear_pedido',
-            data = pedido;
-        $.ajax({
-            url: url,
-            type: type,
-            data: data,
-            success: function(response) {
-                window.location = 'http://localhost/tavo_sg/';
-            }
+  });
+  // Funcion para traer categorias
+  $('.modal-body').on('click', '.mesa-libre', function() {
+    var that = $(this),
+        url = 'main/getCategorias',
+        type = 'post',
+        data = {};
+
+    // Guardando el ID de la mesa seleccionada
+    pedido.id_mesa = that.data('id');
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      success: function(response) {
+        categorias = response; // Llenamos la variable con las categorias
+
+        var template = $('#categorias-template').html(),
+            btnTemplate = $('#btn-completar').html();
+
+        $('.modal-body').html(""); // Borramos el modal
+        $.each(response, function(index, value) {
+            $('.modal-body').append(Mustache.render(template, response[index]));
         });
+        $('.modal-footer').html(""); // Borramos el footer del modal
+        $('.modal-footer').append(Mustache.render(btnTemplate)); // Agregamos el btn de completar
+      }
     });
+  });
+
+  // Traer los productos de una categoria
+  $('.modal-body').on('click', '.categoria', function() {
+    var that = $(this),
+        url = 'main/getProductosXCategoria',
+        type = 'post',
+        data = {};
+
+    data['id_cat'] = $(this).data('id'); // Guardamos ID de la categoria seleccionada
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      success: function(productos) {
+    		// Cargamos las plantillas
+        var template = $('#productos-template').html(),
+            back = $('#back-button').html();
+
+        $('.modal-body').html(""); // Borramos el contenido del modal
+        $('.modal-body').append(back); // Colocamos el btn "atras"
+        $.each(productos, function(index, value) {
+            $('.modal-body').append(Mustache.render(template, productos[index]));
+        });
+      }
+    });
+  });
+
+  // Agregar producto al pedido
+  $('.modal-body').on('click', '.btn-agregar', function() {
+      var that = $(this),
+          flag = 0,
+          cantidad = that.closest('.number-wrapper').find('.cantidad'),
+          valor = parseInt(cantidad.text());
+
+      cantidad.html(""); // Borramos posibles cantidades anteriores
+
+      if (pedido.productos == '') {
+          pedido.productos.push({
+              "id": that.data('id'),
+              "cantidad": "1"
+          });
+          cantidad.append("1");
+      } else {
+          // Buscamos el ID en el arreglo de productos,
+          // si existe, se le suma uno a la cantidad.
+          $.each(pedido.productos, function(index, value) {
+              // Si el ID del producto existe en el arreglo
+              // sumale uno a la cantidad
+              if (pedido.productos[index].id === that.data('id')) {
+                  pedido.productos[index].cantidad = parseInt(pedido.productos[index].cantidad) + 1;
+                  pedido.productos[index].id = that.data('id');
+                  cantidad.append(valor + 1);
+                  flag = 1;
+              }
+          });
+          // Si el ID no existe en el arreglo, agregamos un
+          // producto nuevo
+          if (flag == 0) {
+              pedido.productos.push({
+                  "id": that.data('id'),
+                  "cantidad": "1"
+              });
+              cantidad.append("1");
+          }
+      }
+  });
+
+	// Volver al menu de categorias
+  $('.modal-body').on('click', '.btn-atras', function() {
+    var template = $('#categorias-template').html(),
+    		json = $.merge(categorias, pedido);
+
+    $('.modal-body').html(""); // Borramos el contenido del modal
+    $.each(categorias, function(index, value) {
+        $('.modal-body').append(Mustache.render(template, json[index]));
+    });
+  });
+
+  // Completar y generar la orden
+  $('.modal-footer').on('click', '.btn-completar', function() {
+    var type = 'post',
+        url = 'main/crear_pedido',
+        data = pedido;
+
+    $.ajax({
+      url: url,
+      type: type,
+      data: data,
+      success: function(response) {
+        window.location = 'http://localhost/tavo_sg/';
+      }
+    });
+  });
 }
